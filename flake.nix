3ee38618adf9c68be6041f76f7c0b61ec5c38303
Omer Naveed <me@omernaveed.dev>
2024-06-11 01:23:44 -0500

    flake.nix: Add `flake-parts` to Nix Flake inputs
    `flake-parts` uses the NixOS module system to enable refactoring the
    flake output attribute into modules that are more easily defined and
    reduces the need for 'glue code' such as `flake-utils`.
    
    REF: https://flake.parts/
    REF: https://ayats.org/blog/no-flake-utils/

--- flake.nix
+++ flake.nix
@@ -1,16 +1,25 @@
 {
   inputs = {
+    # Simplify Nix Flakes with the module system
+    flake-parts.url = "github:hercules-ci/flake-parts";
+
     # Nix Packages collection & NixOS
     nixpkgs.url = "github:nixos/nixpkgs/nixos-24.05";
   };
 
   outputs =
-    { self, nixpkgs }:
-    {
-      packages.x86_64-linux.hello = nixpkgs.legacyPackages.x86_64-linux.hello;
+    inputs@{ flake-parts, ... }:
+    flake-parts.lib.mkFlake { inherit inputs; } {
+      systems = [ "x86_64-linux" ];
 
-      packages.x86_64-linux.default = self.packages.x86_64-linux.hello;
+      perSystem =
+        { config, pkgs, ... }:
+        {
+          packages.hello = pkgs.hello;
 
-      formatter.x86_64-linux = nixpkgs.legacyPackages.x86_64-linux.nixfmt-rfc-style;
+          packages.default = config.packages.hello;
+
+          formatter = pkgs.nixfmt-rfc-style;
+        };
     };
 }
