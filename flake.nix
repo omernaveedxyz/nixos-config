f289f63098f05c93391f5cf08d97b0ea50c155cf
Omer Naveed <me@omernaveed.dev>
2024-06-11 15:21:25 -0500

    flake.nix: Add `ez-configs` to Nix Flake inputs
    `ez-configs` allows for defining multiple NixOS configurations with
    reusable common modules paired with `flake-parts`.
    
    REF: https://flake.parts/options/ez-configs

--- flake.nix
+++ flake.nix
@@ -1,26 +1,45 @@
 {
   inputs = {
+    # Nix Packages collection & NixOS
+    nixpkgs.url = "github:nixos/nixpkgs/nixos-24.05";
+
     # Simplify Nix Flakes with the module system
     flake-parts.url = "github:hercules-ci/flake-parts";
 
-    # Nix Packages collection & NixOS
-    nixpkgs.url = "github:nixos/nixpkgs/nixos-24.05";
+    # A flake-parts module for simple nixos, darwin and home-manager configurations
+    ez-configs.url = "github:ehllie/ez-configs";
+    ez-configs.inputs.nixpkgs.follows = "nixpkgs";
   };
 
   outputs =
-    inputs@{ flake-parts, ... }:
+    inputs@{
+      self,
+      flake-parts,
+      ez-configs,
+      ...
+    }:
     flake-parts.lib.mkFlake { inherit inputs; } {
       systems = [ "x86_64-linux" ];
+
+      imports = [ ez-configs.flakeModule ];
+
+      ezConfigs = {
+        # The root from which configurations and modules should be searched
+        root = ./.;
+
+        # Extra arguments to pass to all configurations
+        globalArgs = {
+          inherit inputs;
+        };
+      };
 
       perSystem =
         { config, pkgs, ... }:
         {
-          packages.hello = pkgs.hello;
-
-          packages.default = config.packages.hello;
-
+          # Run a bash shell that provides the build environment of a derivation
           devShells.default = pkgs.mkShell { packages = with pkgs; [ ]; };
 
+          # Reformat your code in the standard style
           formatter = pkgs.nixfmt-rfc-style;
         };
     };
