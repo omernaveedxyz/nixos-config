3fe13206c1b4ae67b5e14854d3ddc6e1a4ff5204
Omer Naveed <me@omernaveed.dev>
2024-06-11 00:42:38 -0500

    flake.nix: Pin Nixpkgs input to 24.05 channel
    Pin the Nixpkgs input to the 'nixos-24.05' branch from the
    [nixpkgs](https://github.com/NixOS/nixpkgs) repository.
    
    REF: https://nixos.wiki/wiki/Nixpkgs

--- flake.nix
+++ flake.nix
@@ -1,6 +1,7 @@
 {
   inputs = {
-    nixpkgs.url = "github:nixos/nixpkgs?ref=nixos-unstable";
+    # Nix Packages collection & NixOS
+    nixpkgs.url = "github:nixos/nixpkgs/nixos-24.05";
   };
 
   outputs = { self, nixpkgs }: {
