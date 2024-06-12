b28eb751535c511b2c3f2a4d00355246b2f31cc2
Omer Naveed <me@omernaveed.dev>
2024-06-12 01:15:51 -0500

    nixos-configurations/omer-laptop: Add comments

--- nixos-configurations/omer-laptop/default.nix
+++ nixos-configurations/omer-laptop/default.nix
@@ -15,6 +15,7 @@
     nixos-hardware.nixosModules.common-gpu-nvidia-disable
   ];
 
+  # The set of kernel modules in the initial ramdisk used during the boot process
   boot.initrd.availableKernelModules = [
     "xhci_pci"
     "ahci"
@@ -23,12 +24,20 @@
     "sd_mod"
     "rtsx_pci_sdmmc"
   ];
+
+  # List of modules that are always loaded by the initrd
   boot.initrd.kernelModules = [ ];
+
+  # The set of kernel modules to be loaded in the second stage of the boot process
   boot.kernelModules = [ "kvm-intel" ];
+
+  # A list of additional packages supplying kernel modules
   boot.extraModulePackages = [ ];
 
+  # Specifies the platform where the NixOS configuration will run
   nixpkgs.hostPlatform = lib.mkDefault "x86_64-linux";
 
+  # The name of the machine
   networking.hostName = "omer-laptop";
 
   fileSystems."/" = {
