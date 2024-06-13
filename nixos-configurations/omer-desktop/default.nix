e5dc88c720e7e8d00ee02a0c4af16811f7d22d36
Omer Naveed <me@omernaveed.dev>
2024-06-13 00:03:29 -0500

    nixos-configurations/omer-desktop: Initial config
    Create initial configuration for 'omer-desktop' based on the generated
    config by the NixOS Installer.

---
+++ nixos-configurations/omer-desktop/default.nix
@@ -0,0 +1,112 @@
+{
+  modulesPath,
+  config,
+  lib,
+  pkgs,
+  nixos-hardware,
+  ...
+}:
+
+{
+  imports = [
+    (modulesPath + "/installer/scan/not-detected.nix")
+
+    nixos-hardware.nixosModules.common-cpu-amd
+    nixos-hardware.nixosModules.common-pc-ssd
+  ];
+
+  # The set of kernel modules in the initial ramdisk used during the boot process
+  boot.initrd.availableKernelModules = [
+    "nvme"
+    "xhci_pci"
+    "ahci"
+    "usbhid"
+    "usb_storage"
+    "sd_mod"
+  ];
+
+  # List of modules that are always loaded by the initrd
+  boot.initrd.kernelModules = [ ];
+
+  # The set of kernel modules to be loaded in the second stage of the boot process
+  boot.kernelModules = [ "kvm-amd" ];
+
+  # A list of additional packages supplying kernel modules
+  boot.extraModulePackages = [ ];
+
+  # Specifies the platform where the NixOS configuration will run
+  nixpkgs.hostPlatform = lib.mkDefault "x86_64-linux";
+
+  # The name of the machine
+  networking.hostName = "omer-desktop";
+
+  fileSystems."/" = {
+    device = "/dev/disk/by-uuid/d79d9ed3-f639-4219-a7b9-08ac0292f92a";
+    fsType = "ext4";
+  };
+
+  fileSystems."/boot" = {
+    device = "/dev/disk/by-uuid/6C96-EA1C";
+    fsType = "vfat";
+    options = [
+      "fmask=0022"
+      "dmask=0022"
+    ];
+  };
+
+  # Set your time zone.
+  time.timeZone = "America/Chicago";
+
+  # Enable the X11 windowing system.
+  services.xserver.enable = true;
+  # Enable the GNOME Desktop Environment.
+  services.xserver.displayManager.gdm.enable = true;
+  services.xserver.desktopManager.gnome.enable = true;
+  # Enable CUPS to print documents.
+  services.printing.enable = true;
+  # Enable automatic login for the user.
+  services.xserver.displayManager.autoLogin.enable = true;
+  services.xserver.displayManager.autoLogin.user = "omer";
+  # Workaround for GNOME autologin: https://github.com/NixOS/nixpkgs/issues/103746#issuecomment-945091229
+  systemd.services."getty@tty1".enable = false;
+  systemd.services."autovt@tty1".enable = false;
+
+  # Enable sound with pipewire.
+  hardware.pulseaudio.enable = false;
+  security.rtkit.enable = true;
+  services.pipewire = {
+    enable = true;
+    alsa.enable = true;
+    alsa.support32Bit = true;
+    pulse.enable = true;
+  };
+
+  # Define a user account. Don't forget to set a password with ‘passwd’.
+  users.users.omer = {
+    isNormalUser = true;
+    description = "Omer Naveed";
+    extraGroups = [
+      "networkmanager"
+      "wheel"
+    ];
+    packages = with pkgs; [
+      #  thunderbird
+    ];
+  };
+
+  # Install firefox.
+  programs.firefox.enable = true;
+
+  programs.neovim = {
+    enable = true;
+    defaultEditor = true;
+  };
+  programs.git.enable = true;
+  services.udev.packages = [ pkgs.yubikey-personalization ];
+  programs.gnupg.agent = {
+    enable = true;
+    enableSSHSupport = true;
+  };
+  services.pcscd.enable = true;
+  networking.hostId = "12345678";
+}
