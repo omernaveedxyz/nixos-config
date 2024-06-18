8bff086b72096beb477c3c0c29e63974210ad012
Omer Naveed <me@omernaveed.dev>
2024-06-18 00:55:58 -0500

    home-modules/programs/swaylock.nix: Customize

--- home-modules/programs/swaylock.nix
+++ home-modules/programs/swaylock.nix
@@ -1,6 +1,19 @@
+{ lib, ... }:
 {
   programs.swaylock = {
     # Whether to enable swaylock
     enable = true;
+
+    # Default arguments to swaylock
+    settings = {
+      # Sets the indicator to show even if idle
+      indicator-idle-visible = true;
+
+      # Sets the indicator radius
+      indicator-radius = 120;
+
+      # Sets the color of the inside of the indicator
+      inside-color = lib.mkForce "00000000";
+    };
   };
 }
