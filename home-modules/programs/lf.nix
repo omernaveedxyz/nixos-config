d01291c7f6f47a11199d6eb422cf1f204f4d5a50
Omer Naveed <me@omernaveed.dev>
2024-06-19 23:36:14 -0500

    home-modules/programs/lf.nix: Add keybind
    Add shortcut keybind in Sway to open lf in the Foot terminal.

--- home-modules/programs/lf.nix
+++ home-modules/programs/lf.nix
@@ -1,6 +1,25 @@
 {
+  config,
+  lib,
+  pkgs,
+  ...
+}:
+let
+  inherit (lib) mkIf mkOptionDefault getExe;
+in
+{
   programs.lf = {
     # Whether to enable lf
     enable = true;
+  };
+
+  wayland.windowManager.sway = mkIf (config.wayland.windowManager.sway.enable) {
+    # Sway configuration options
+    config = {
+      # An attribute set that assigns a key press to an action using a key symbol
+      keybindings = mkOptionDefault {
+        "${config.wayland.windowManager.sway.config.modifier}+Shift+f" = "exec ${getExe config.programs.foot.package} -e lf";
+      };
+    };
   };
 }
