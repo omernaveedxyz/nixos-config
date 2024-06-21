c8ca8f0f13b8dec2d99b1934594d8ee31b526f02
Omer Naveed <me@omernaveed.dev>
2024-06-21 15:18:04 -0500

    home-configurations/omer: Add custom module args
    Add custom arguments using the `_module.args` Home-Manager property.
    These will be used to enable/disable different programs/configurations
    in Home-Manager.

--- home-configurations/omer/default.nix
+++ home-configurations/omer/default.nix
@@ -4,6 +4,14 @@
     ./programs/stylix
   ];
 
+  # Additional arguments passed to each module
+  _module.args = {
+    impermanence = true;
+    desktop = "sway";
+    terminal = "foot";
+    browser = "firefox";
+  };
+
   # The user’s username
   home.username = "omer";
 }
