155c529f93ac6cff634f5cbf653537247f13cfbe
Omer Naveed <me@omernaveed.dev>
2024-06-21 15:19:45 -0500

    home-modules/programs/foot/default.nix: Update
    Update configuration to source from `_module.args` in order to enable
    the Foot terminal.

--- home-modules/programs/foot/default.nix
+++ home-modules/programs/foot/default.nix
@@ -1,6 +1,18 @@
+{ config, lib, ... }:
+let
+  inherit (lib) mkIf getExe;
+in
 {
   programs.foot = {
     # Whether to enable Foot terminal
-    enable = true;
+    enable = config._module.args.terminal == "foot";
+
+    # Whether to enable Foot terminal server
+    server.enable = true;
+  };
+
+  # Environment variables to always set at login
+  home.sessionVariables = mkIf (config.programs.foot.enable) {
+    TERMINAL = "${getExe config.programs.alacritty.package}";
   };
 }
