5dd8bfeddb5f35ff3240873a9733f95e5a90ff3f
Omer Naveed <me@omernaveed.dev>
2024-06-21 15:26:29 -0500

    home-modules/overlays/default.nix: Flameshot
    Add custom overlay for Flameshot in order to build with WLRoots support
    using the Grim backend instead of the default KDE based backend.

--- home-modules/overlays/default.nix
+++ home-modules/overlays/default.nix
@@ -7,5 +7,20 @@
     # example = prev.example.overrideAttrs (oldAttrs: rec {
     # ...
     # });
+
+    # REF: https://github.com/NixOS/nixpkgs/issues/292700
+    flameshot = prev.flameshot.overrideAttrs (oldAttrs: {
+      src = final.fetchFromGitHub {
+        owner = "flameshot-org";
+        repo = "flameshot";
+        rev = "3d21e4967b68e9ce80fb2238857aa1bf12c7b905";
+        sha256 = "sha256-OLRtF/yjHDN+sIbgilBZ6sBZ3FO6K533kFC1L2peugc=";
+      };
+      cmakeFlags = [
+        "-DUSE_WAYLAND_CLIPBOARD=1"
+        "-DUSE_WAYLAND_GRIM=1"
+      ];
+      buildInputs = oldAttrs.buildInputs ++ [ final.libsForQt5.kguiaddons ];
+    });
   };
 }
