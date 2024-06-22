4f4fd84c3481e31fe4faf5d46f9f509732074e2f
Omer Naveed <me@omernaveed.dev>
2024-06-22 17:48:35 -0500

    home-modules/services/swaync: Add keybind
    Add shortcut to open the Sway Notification Center in Sway with the
    keybind "ALT + SHIFT + N".

--- home-modules/services/swaync/default.nix
+++ home-modules/services/swaync/default.nix
@@ -1,6 +1,22 @@
+{ config, lib, ... }:
+let
+  inherit (lib) mkIf mkOptionDefault getExe;
+in
 {
   services.swaync = {
     # Whether to enable Swaync notification daemon
     enable = true;
   };
+
+  wayland.windowManager.sway =
+    mkIf (config.wayland.windowManager.sway.enable && config.services.swaync.enable)
+      {
+        # Sway configuration options
+        config = {
+          # An attribute set that assigns a key press to an action using a key symbol
+          keybindings = mkOptionDefault {
+            "${config.wayland.windowManager.sway.config.modifier}+Shift+n" = "exec ${getExe config.services.swaync.package}-client -t -sw";
+          };
+        };
+      };
 }
