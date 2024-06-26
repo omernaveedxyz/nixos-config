6003e532338dcdc59acaf02dc59ed33ee72d2629
Omer Naveed <me@omernaveed.dev>
2024-06-26 00:14:33 -0500

    home-modules/services/wob/default.nix: Configure
    Configure anchor point and margin for Wob.

--- home-modules/services/wob/default.nix
+++ home-modules/services/wob/default.nix
@@ -4,7 +4,15 @@
     enable = true;
 
     # Configuration written to $XDG_CONFIG_HOME/wob/wob.ini
-    settings = { };
+    settings = {
+      "" = {
+        # Anchor point, combination of top, left, right, bottom, center
+        anchor = "bottom";
+
+        # Anchor  margin,  in pixels. Either as a single value or 4 values (top right bottom left)
+        margin = "0 0 50 0";
+      };
+    };
 
     # Whether to enable systemd service and socket for wob
     systemd = true;
