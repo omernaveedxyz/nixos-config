056d8166ffbdf6fd33741e519e6d1e24f60f5648
Omer Naveed <me@omernaveed.dev>
2024-06-30 18:48:40 -0500

    home-configurations/omer/programs/stylix: Update
    Switch to another theme and a different wallpaper.

--- home-configurations/omer/programs/stylix/default.nix
+++ home-configurations/omer/programs/stylix/default.nix
@@ -10,7 +10,10 @@ in
 {
   stylix = mkIf (config.stylix.enable) {
     # A scheme following the base16 standard
-    base16Scheme = "${pkgs.base16-schemes}/share/themes/helios.yaml";
+    # base16Scheme = "${pkgs.base16-schemes}/share/themes/nord.yaml";
+
+    # Use this option to force a light or dark theme
+    polarity = "dark";
 
     # Wallpaper image
     image = ./wallpaper.png;
@@ -20,7 +23,7 @@ in
       package = pkgs.bibata-cursors;
 
       # The cursor name within the package
-      name = "Bibata-Modern-Amber";
+      name = "Bibata-Modern-Classic";
 
       # The cursor size
       size = 24;
