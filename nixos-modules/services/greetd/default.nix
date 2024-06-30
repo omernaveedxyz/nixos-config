2b0fcb3baba80a2d8c8d3a75718e361b58a8ef4f
Omer Naveed <me@omernaveed.dev>
2024-06-30 17:49:02 -0500

    nixos-modules/services/greetd: Update tuigreet cmd
    Update tuigreet command to remember the last selected user and last
    selected session for each user instead of having a default.

--- nixos-modules/services/greetd/default.nix
+++ nixos-modules/services/greetd/default.nix
@@ -1,6 +1,11 @@
-{ lib, pkgs, ... }:
+{
+  config,
+  lib,
+  pkgs,
+  ...
+}:
 let
-  inherit (lib) getExe;
+  inherit (lib) getExe mkIf;
 in
 {
   services.greetd = {
@@ -10,9 +15,22 @@ in
     # Greetd configuration
     settings = {
       default_session = {
-        command = "${getExe pkgs.greetd.tuigreet} --time --cmd Hyprland";
+        command = "${getExe pkgs.greetd.tuigreet} --time --remember --remember-user-session";
         user = "greeter";
       };
     };
+  };
+
+  # Files and directories to persistent across ephemeral boots
+  environment.persistence."/persistent" = mkIf (config._module.args.impermanence) {
+    # All directories you want to link or bind to persistent storage
+    directories = [
+      {
+        directory = "/var/cache/tuigreet";
+        user = "tuigreet";
+        group = "tuigreet";
+        mode = "0755";
+      }
+    ];
   };
 }
