8649c3c0a4906465c24df625c8d5d405285432f8
Omer Naveed <me@omernaveed.dev>
2024-07-01 15:00:19 -0500

    nixos-modules/services/pipewire: Update bluetooth
    After merge of nixos/nixpkgs#292115, Wireplumber now has the
    `extraConfig` field available.

--- nixos-modules/services/pipewire/default.nix
+++ nixos-modules/services/pipewire/default.nix
@@ -22,18 +22,20 @@ in
     # Whether to enable PulseAudio server emulation
     pulse.enable = true;
 
-    # List of packages the provide WirePlumber configuration
-    # TODO: https://github.com/NixOS/nixpkgs/pull/292115
-    wireplumber.configPackages = mkIf (config.hardware.bluetooth.enable) [
-      (writeTextDir "share/wireplumber/bluetooth.lua.d/51-bluez-config.lua" ''
-        bluez_monitor.properties = {
-          ["bluez5.enable-sbc-xq"] = true,
-          ["bluez5.enable-msbc"] = true,
-          ["bluez5.enable-hw-volume"] = true,
-          ["bluez5.headset-roles"] = "[hsp_hs hsp_ag hfp_hf hfp_ag]"
-        }
-      '')
-    ];
+    # Additional configuration for the WirePlumber daemon when run in single-instance mode
+    wireplumber.extraConfig = mkIf (config.hardware.bluetooth.enable) {
+      "monitor.bluez.properties" = {
+        "bluez5.enable-sbc-xq" = true;
+        "bluez5.enable-msbc" = true;
+        "bluez5.enable-hw-volume" = true;
+        "bluez5.roles" = [
+          "hsp_hs"
+          "hsp_ag"
+          "hfp_hf"
+          "hfp_ag"
+        ];
+      };
+    };
   };
 
   # Whether to enable the RealtimeKit system service, which hands out realtime
