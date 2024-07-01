9a001c1aa82aedb044596ceae8599be9a88be671
Omer Naveed <me@omernaveed.dev>
2024-06-30 23:20:15 -0500

    home-modules/programs/fzf: remove option
    Remove `enableBashIntegration` option as it is `true` by default.

--- home-modules/programs/fzf/default.nix
+++ home-modules/programs/fzf/default.nix
@@ -2,8 +2,5 @@
   programs.fzf = {
     # Whether to enable fzf - a command-line fuzzy finder
     enable = true;
-
-    # Whether to enable Bash integration
-    enableBashIntegration = true;
   };
 }
