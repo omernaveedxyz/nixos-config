f85aa44ed6d929870b4de94dfa36107c30763c73
Omer Naveed <me@omernaveed.dev>
2024-07-03 03:09:22 -0500

    home-modules/programs/bat: Add syntax
    Add additional syntax to enable highlighting for Just files.

--- home-modules/programs/bat/default.nix
+++ home-modules/programs/bat/default.nix
@@ -1,6 +1,20 @@
+{ pkgs, ... }:
 {
   programs.bat = {
     # Whether to enable bat, a cat clone with wings
     enable = true;
+
+    # Additional syntaxes to provide
+    syntaxes = {
+      just = {
+        src = pkgs.fetchFromGitHub {
+          owner = "nk9";
+          repo = "just_sublime";
+          rev = "352bae277961d41e2a1795a834dbf22661c8910f";
+          hash = "sha256-QCp6ypSBhgGZG4T7fNiFfCgZIVJoDSoJBkpcdw3aiuQ=";
+        };
+        file = "Syntax/Just.sublime-syntax";
+      };
+    };
   };
 }
