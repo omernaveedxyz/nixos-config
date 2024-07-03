ad4d05aca435f73017ce276309c308eb758b28cd
Omer Naveed <me@omernaveed.dev>
2024-07-02 23:56:19 -0500

    home-modules/overlays/pkgs: Add TODO
    Add 'TODO' comment to track using proton-pass overlay in Home-Manager.

--- home-modules/overlays/pkgs/default.nix
+++ home-modules/overlays/pkgs/default.nix
@@ -1,5 +1,5 @@
 # Custom packages, that can be defined similarly to ones from Nixpkgs
 pkgs: {
   # example = pkgs.callPackage ./example { };
-  proton-pass = pkgs.callPackage ./proton-pass { };
+  proton-pass = pkgs.callPackage ./proton-pass { }; # TODO: https://github.com/nix-community/home-manager/issues/5599
 }
