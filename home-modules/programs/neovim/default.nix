f694665998f1df61b282d3056fbe192f48a82f55
Omer Naveed <me@omernaveed.dev>
2024-07-06 01:15:30 -0500

    home-modules/programs/neovim: Add completions
    Enable completions using nvim-cmp, luasnip, and friendly-snippets.

--- home-modules/programs/neovim/default.nix
+++ home-modules/programs/neovim/default.nix
@@ -198,6 +198,69 @@ in
         # Whether to enable tree-sitter syntax highlighting
         enable = true;
       };
+
+      # A completion plugin for neovim coded in Lua
+      cmp = {
+        # Whether to enable nvim-cmp
+        enable = true;
+
+        # Options provided to the require('cmp').setup function
+        settings = {
+          # The sources to use
+          sources = [
+            { name = "nvim_lsp"; }
+            { name = "luasnip"; }
+            { name = "path"; }
+            { name = "buffer"; }
+          ];
+
+          # The snippet expansion function
+          snippet.expand = {
+            __raw = "function(args) require('luasnip').lsp_expand(args.body) end";
+          };
+
+          # Cmp mappings declaration
+          mapping = {
+            __raw = ''
+              cmp.mapping.preset.insert({
+                -- Select the [n]ext item
+                ['<C-n>'] = cmp.mapping.select_next_item(),
+                -- Select the [p]revious item
+                ['<C-p>'] = cmp.mapping.select_prev_item(),
+
+                -- Scroll the documentation window [b]ack / [f]orward
+                ['<C-b>'] = cmp.mapping.scroll_docs(-4),
+                ['<C-f>'] = cmp.mapping.scroll_docs(4),
+
+                -- Accept ([y]es) the completion.
+                --  This will auto-import if your LSP supports it.
+                --  This will expand snippets if the LSP sent a snippet.
+                ['<C-y>'] = cmp.mapping.confirm { select = true },
+
+                -- Manually trigger a completion from nvim-cmp.
+                --  Generally you don't need this, because nvim-cmp will display
+                --  completions whenever it has completion options available.
+                ['<C-Space>'] = cmp.mapping.complete {},
+              })
+            '';
+          };
+        };
+      };
+
+      # Snippet Engine for Neovim written in Lua
+      luasnip = {
+        # Whether to enable luasnip
+        enable = true;
+
+        # List of custom vscode style snippets to load
+        fromVscode = [ { } ];
+      };
+
+      # Set of preconfigured snippets for different languages
+      friendly-snippets = {
+        # Whether to enable friendly-snippets
+        enable = true;
+      };
     };
 
     # Autocmd definitions
