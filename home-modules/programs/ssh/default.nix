a08b154d33c6e6a748328ba37ad01a0e5bf43aff
Omer Naveed <me@omernaveed.dev>
2024-07-15 16:58:20 -0500

    home-modules/programs/ssh: Add remote forwarding
    Add support for agent forwarding over SSH using the GnuPG agent.

--- home-modules/programs/ssh/default.nix
+++ home-modules/programs/ssh/default.nix
@@ -1,6 +1,30 @@
+{ lib, relativeToRoot, ... }:
+let
+  inherit (builtins) readDir;
+  inherit (lib) listToAttrs attrNames;
+in
 {
   programs.ssh = {
     # Whether to enable SSH client configuration
     enable = true;
+
+    # Specify per-host settings
+    matchBlocks = listToAttrs (
+      map (host: {
+        name = host;
+        value = {
+          # Whether the connection to the authentication agent (if any) will be forwarded to the remote machine
+          forwardAgent = true;
+
+          # Specify remote port forwardings
+          remoteForwards = [
+            {
+              bind.address = "/run/user/1000/gnupg/S.gpg-agent";
+              host.address = "/run/user/1000/gnupg/S.gpg-agent.extra";
+            }
+          ];
+        };
+      }) (attrNames (readDir (relativeToRoot "nixos-configurations")))
+    );
   };
 }
